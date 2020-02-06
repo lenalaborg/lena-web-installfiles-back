5563307ebfbcf5c47e4bc560531909c7b171e25c
원송연 책임/아키텍처솔루션팀 <songyeon.won@lgcns.com>
2020-02-06 15:30:51 +0900

    [202002061530]Update Lena Install File

--- lena-web-linux_na_x86_64-1.3.1ce/depot/lena-web/1.3.1/module/web-vela-server/src/version.h
+++
@@ -1,7 +0,0 @@
-/* $OpenBSD: version.h,v 1.67 2013/07/25 00:57:37 djm Exp $ */
-
-#define SSH_VERSION	"OpenSSH_6.3"
-
-#define SSH_PORTABLE	"p1"
-#define SSH_HPN         "-hpn14v2"
-#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
